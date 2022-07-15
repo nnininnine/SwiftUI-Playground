973981b26610ec4bb2ff90cf00781d26c4534934
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-15 13:45:48 +0700

    feat: create HomeView

---
+++ SwiftUI-Playground/Screens/Home/View/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Playground
+//
+//  Created by 7Peaks on 15/7/2565 BE.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
--- SwiftUI-Playground/SwiftUI_PlaygroundApp.swift
+++ SwiftUI-Playground/SwiftUI_PlaygroundApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct SwiftUI_PlaygroundApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      HomeView()
     }
+  }
 }
