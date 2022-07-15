c4e4e2ff03452a5fc4e3301cbff7691ee9136e1a
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-15 14:13:39 +0700

    feat: create TextFieldView

---
+++ SwiftUI-Playground/Screens/TextField/View/TextFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldView.swift
+//  SwiftUI-Playground
+//
+//  Created by 7Peaks on 15/7/2565 BE.
+//
+
+import SwiftUI
+
+struct TextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldView()
+    }
+}
