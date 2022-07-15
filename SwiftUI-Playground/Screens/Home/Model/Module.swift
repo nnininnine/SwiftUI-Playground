5e165510b2da2abf64946b9d560e80da6a05e1b5
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-15 14:06:06 +0700

    feat: create HomeViewModel, Module model

---
+++ SwiftUI-Playground/Screens/Home/Model/Module.swift
@@ -0,0 +1,15 @@
+//
+//  Module.swift
+//  SwiftUI-Playground
+//
+//  Created by 7Peaks on 15/7/2565 BE.
+//
+
+import SwiftUI
+
+typealias Modules = [Module]
+
+struct Module {
+  let name: String
+  let view: AnyView
+}
---
+++ SwiftUI-Playground/Screens/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  SwiftUI-Playground
+//
+//  Created by 7Peaks on 15/7/2565 BE.
+//
+
+import Combine
+
+class HomeViewModel: ObservableObject {
+  // MARK: Properties
+
+  let modules: Modules = .init()
+}
