de085efe9f12a3369e3fdc2c6d692401dc76ad8e
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-15 13:42:45 +0700

    Initial Commit

---
+++ SwiftUI-Playground/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  SwiftUI-Playground
+//
+//  Created by 7Peaks on 15/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
