b9ef7b7144e7193dd3fcffd9173db9d3063a00e4
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-19 12:19:55 +0700

    feat(shape): implement Circle example

--- SwiftUI-Playground/Screens/Shape/View/ShapeView.swift
+++ SwiftUI-Playground/Screens/Shape/View/ShapeView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct ShapeView: View {
   var body: some View {
-    Text("ShapeView")
+    ScrollView {
+      VStack(alignment: .center, spacing: 4) {
+        Text("Circle")
+          .font(.title2)
+        Circle()
+          .fill(.red)
+          .frame(width: 100, height: 100)
+        Circle()
+          .strokeBorder(.black, style: .init(lineWidth: 6))
+          .frame(width: 100, height: 100)
+        Circle()
+          .strokeBorder(.black, style: .init(lineWidth: 6))
+          .background(Circle().fill(.red))
+          .frame(width: 100, height: 100)
+      }
+      .padding(.horizontal)
+    }
+    .padding(.horizontal)
+    .frame(width: UIScreen.main.bounds.width)
+    .navigationTitle("Shape")
   }
 }
 
