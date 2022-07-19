d760b763b0b6e5197d5cff128a97843dc95e8ee0
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-19 11:50:02 +0700

    feat: create ShapeView

---
+++ SwiftUI-Playground/Screens/Shape/View/ShapeView.swift
@@ -0,0 +1,20 @@
+//
+//  ShapeView.swift
+//  SwiftUI-Playground
+//
+//  Created by 7Peaks (Nine) on 19/7/2565 BE.
+//
+
+import SwiftUI
+
+struct ShapeView: View {
+  var body: some View {
+    Text("ShapeView")
+  }
+}
+
+struct ShapeView_Previews: PreviewProvider {
+  static var previews: some View {
+    ShapeView()
+  }
+}
