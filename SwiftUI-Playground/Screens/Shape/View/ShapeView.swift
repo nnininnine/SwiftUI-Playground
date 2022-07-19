5c797465ee39e77f2ac8a0020024542bd1401776
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-19 12:27:05 +0700

    feat(shape): implement Ellipse example

--- SwiftUI-Playground/Screens/Shape/View/ShapeView.swift
+++ SwiftUI-Playground/Screens/Shape/View/ShapeView.swift
@@ -10,21 +10,39 @@ import SwiftUI
 struct ShapeView: View {
   var body: some View {
     ScrollView {
-      VStack(alignment: .center, spacing: 4) {
-        Text("Circle")
-          .font(.title2)
-        Circle()
-          .fill(.red)
-          .frame(width: 100, height: 100)
-        Circle()
-          .strokeBorder(.black, style: .init(lineWidth: 6))
-          .frame(width: 100, height: 100)
-        Circle()
-          .strokeBorder(.black, style: .init(lineWidth: 6))
-          .background(Circle().fill(.red))
-          .frame(width: 100, height: 100)
-      }
-      .padding(.horizontal)
+      // MARK: Circle
+
+      Text("Circle")
+        .font(.title2)
+      Circle()
+        .fill(.red)
+        .frame(width: 100, height: 100)
+      Circle()
+        .strokeBorder(.black, style: .init(lineWidth: 6))
+        .frame(width: 100, height: 100)
+      Circle()
+        .strokeBorder(.black, style: .init(lineWidth: 6))
+        .background(Circle().fill(.red))
+        .frame(width: 100, height: 100)
+
+      Divider()
+
+      // MARK: Ellipse
+
+      Text("Ellipse")
+        .font(.title2)
+      Ellipse()
+        .fill(.yellow)
+        .frame(width: 200, height: 100)
+      Ellipse()
+        .strokeBorder(.blue, style: .init(lineWidth: 6))
+        .frame(width: 200, height: 100)
+      Ellipse()
+        .strokeBorder(.blue, style: .init(lineWidth: 6))
+        .background(Ellipse().fill(.yellow))
+        .frame(width: 200, height: 100)
+
+      Divider()
     }
     .padding(.horizontal)
     .frame(width: UIScreen.main.bounds.width)
